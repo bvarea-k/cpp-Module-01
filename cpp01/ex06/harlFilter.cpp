c8e0dc3e4dc4fbb46988fe1aaf6592fd06db41a0
bvarea-k <bvarea-k@student.42malaga.com>
2025-11-25 13:09:00 +0100

    Update

--- cpp01/ex06/harlFilter.cpp
+++ cpp01/ex06/harlFilter.cpp
@@ -6,7 +6,7 @@
 /*   By: bvarea-k <bvarea-k@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 13:17:13 by bvarea-k          #+#    #+#             */
-/*   Updated: 2025/11/25 12:02:51 by bvarea-k         ###   ########.fr       */
+/*   Updated: 2025/11/25 12:08:53 by bvarea-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,21 @@ void Harl::complainFilter(std::string level) {
 }
 
 void	Harl::debug( void ) {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon..." << std::endl;
 }
 
 void	Harl::info( void ) {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void ) {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void	Harl::error( void ) {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bvarea-k <bvarea-k@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 13:16:51 by bvarea-k          #+#    #+#             */
-/*   Updated: 2025/11/25 12:03:25 by bvarea-k         ###   ########.fr       */
+/*   Updated: 2025/11/25 12:12:52 by bvarea-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	main(int ac, char **av) {
 	
 	if (ac == 2)
 		harl.complainFilter(av[1]);
+	else
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	return (0);
 }
 
