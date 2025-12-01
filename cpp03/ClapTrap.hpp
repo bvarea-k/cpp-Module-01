071be9ebafa147f8e6ad8632fc6a36b1f5d1e10c
bvarea-k <bvarea-k@student.42malaga.com>
2025-12-01 13:06:46 +0100

    cpp03 added

---
+++ cpp03/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bvarea-k <bvarea-k@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 11:02:26 by bvarea-k          #+#    #+#             */
+/*   Updated: 2025/12/01 11:55:39 by bvarea-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap {
+	private:
+		std::string	_name;
+		int _hitPoints;
+		int	_energyPoints;
+		int	_attackDamage;
+
+	public:
+		ClapTrap();									
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &other);
+		~ClapTrap();
+		
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
